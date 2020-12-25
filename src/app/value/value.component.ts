083591764b975bbf36477ada9ca062296e9630ef
Gagana Gavesh <gaveshgamage@gmail.com>
2020-12-25 23:49:33 +0530

    Get values moved to register component

--- src/app/value/value.component.ts
+++ src/app/value/value.component.ts
@@ -14,17 +14,17 @@ export class ValueComponent implements OnInit {
   constructor(private http: Http) { }
 
   ngOnInit(): void {
-    this.getValues();
+    //this.getValues();
   }
 
-  getValues(){
-    this.http.get('http://localhost:50100/api/values').subscribe(
-      res => {
-        this.values = res.json();//Attempts to return body as parsed JSON object, or raises an exception
-        console.log(this.values);
-      }, error=>{
-        console.log(error);
-      });
-  }
+  // getValues(){
+  //   this.http.get('http://localhost:50100/api/values').subscribe(
+  //     res => {
+  //       this.values = res.json();//Attempts to return body as parsed JSON object, or raises an exception
+  //       console.log(this.values);
+  //     }, error=>{
+  //       console.log(error);
+  //     });
+  // }
 
 }
