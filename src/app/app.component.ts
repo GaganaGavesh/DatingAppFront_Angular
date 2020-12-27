82ff64f00616c21761229dd1df95de73e475c572
Gagana Gavesh <gaveshgamage@gmail.com>
2020-12-27 22:48:12 +0530

    issue fixed loading name of already logged in user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JwtHelper } from 'angular2-jwt';
+import { AuthService } from './_services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'DatingAppSPA';
+  jwtHelper: JwtHelper = new JwtHelper();
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(){
+    //meka damme token eka tyna welawatta userge name eka ganne nathi eka hadanna
+    //mokada methanama damme ?? 
+    //me component eka thama highest level component eka
+    //refresh waladi aniwa wada karanawa 
+    //methendima check karala token ekak tyenam authservice eke variable ekata dagannawa
+    //ethakota refresh waladi aniwa token name eka ganna than walata me name eka ganna pluwan aulak nathuwa
+    const token = localStorage.getItem('token');
+    if(token){
+      this.authService.decodedToken = this.jwtHelper.decodeToken(token);
+    }
+  }
 }
