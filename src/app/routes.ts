f457ab9d88c85a6ee5bca92d6785ffb1c8a253c5
Gagana Gavesh <gaveshgamage@gmail.com>
2020-12-31 17:18:26 +0530

    Route Guard added to the routes

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,13 +3,21 @@ import { HomeComponent } from "./home/home.component";
 import { ListComponent } from "./list/list.component";
 import { MemberListComponent } from "./member-list/member-list.component";
 import { MessagesComponent } from "./messages/messages.component";
+import { AuthGuard } from "./_guards/auth.guard";
 
 //appRoutes kiyanne router object tyna array ekak meke Routes kiyana thanata 
 //Routes[] kiyala dammath aulak ne
 export const appRoutes : Routes =[
     { path : 'home', component : HomeComponent },
-    { path : 'members', component : MemberListComponent },
-    { path : 'messages', component : MessagesComponent },
-    { path : 'lists', component : ListComponent },
+    {
+      path: '',//me path eka athule balanwa, habai children la match wenawada balanne AuthGuard eka access dunnoth witharai
+      runGuardsAndResolvers: 'always',
+      canActivate: [AuthGuard],
+      children: [
+        { path : 'members', component : MemberListComponent },
+        { path : 'messages', component : MessagesComponent },
+        { path : 'lists', component : ListComponent }
+      ]
+    },
     { path : '**', redirectTo : 'home', pathMatch : 'full'},
 ]
