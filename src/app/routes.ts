b8acf80a3f152a457105e0709b90812d81c86235
Gagana Gavesh <gaveshgamage@gmail.com>
2021-01-18 15:49:32 +0530

    PreventUnsavedChangesGuard added to protect the unsaved data

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from "@angular/router";
+import { UnsubscriptionError } from "rxjs";
 import { HomeComponent } from "./home/home.component";
 import { ListComponent } from "./list/list.component";
 import { MemberDetailComponent } from "./members/member-detail/member-detail.component";
@@ -6,6 +7,7 @@ import { MemberEditComponent } from "./members/member-edit/member-edit.component
 import { MemberListComponent } from "./members/member-list/member-list.component";
 import { MessagesComponent } from "./messages/messages.component";
 import { AuthGuard } from "./_guards/auth.guard";
+import { PreventUnsavedChangesGuard } from "./_guards/prevent-unsaved-changes.guard";
 import { MemberDetailResolver } from "./_resolvers/member-detail.resolver";
 import { MemberEditResolver } from "./_resolvers/member-edit.resolver";
 import { MemberListResolver } from "./_resolvers/member-list.resolver";
@@ -21,7 +23,10 @@ export const appRoutes : Routes =[
       children: [
         { path : 'members', component : MemberListComponent, resolve : {users : MemberListResolver} },
         { path : 'members/:id', component : MemberDetailComponent, resolve : {user : MemberDetailResolver}},
-        { path : 'member/edit', component : MemberEditComponent, resolve : {user : MemberEditResolver}},
+        { path : 'member/edit', 
+          component : MemberEditComponent, 
+          resolve : {user : MemberEditResolver},
+          canDeactivate : [PreventUnsavedChangesGuard]},
         { path : 'messages', component : MessagesComponent },
         { path : 'lists', component : ListComponent }
       ]
