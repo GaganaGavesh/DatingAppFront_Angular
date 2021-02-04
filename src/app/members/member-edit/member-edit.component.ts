4974c014dac142e3356f85f10783b828ca8b85a4
Gagana Gavesh <gaveshgamage@gmail.com>
2021-02-04 23:23:29 +0530

    subscribe to the main photo vchanges listner in the authService

--- src/app/members/member-edit/member-edit.component.ts
+++ src/app/members/member-edit/member-edit.component.ts
@@ -15,6 +15,7 @@ export class MemberEditComponent implements OnInit {
 
   user : User;
   @ViewChild('editForm') editform : NgForm; 
+  photoUrl: string;
 
   constructor(
     private route : ActivatedRoute, 
@@ -27,6 +28,7 @@ export class MemberEditComponent implements OnInit {
     this.route.data.subscribe(data =>{
       this.user = data['user'];
     })
+    this.authService.currentPhotoUrl.subscribe(photoUrl => this.photoUrl = photoUrl)
   }
 
   updateUser() {
