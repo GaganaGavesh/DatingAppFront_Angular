0c2a82e0626d35508d0b9d5f3ad79d261bf52994
Gagana Gavesh <gaveshgamage@gmail.com>
2021-02-06 23:38:03 +0530

    Added reactive form to the register section

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit, Output, EventEmitter, ViewChild } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { AlertifyService } from '../_services/alertify.service';
 //import { EventEmitter } from 'events';
 
@@ -16,23 +16,38 @@ export class RegisterComponent implements OnInit {
 
   model : any = {};
   @ViewChild('registerForm') registerForm: NgForm; //to get the form details
+  registerFormReactive : FormGroup;
 
   constructor(private authService : AuthService, private alertify: AlertifyService) { }
 
   ngOnInit(): void {
+    this.registerFormReactive = new FormGroup({
+      username: new FormControl('Gagana', Validators.required),
+      password: new FormControl('', [
+        Validators.required, 
+        Validators.minLength(4), 
+        Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', Validators.required)
+    }, this.passwordMatchValidator);
+  }
+
+  passwordMatchValidator(g : FormGroup){
+    return g.get('password').value === g.get('confirmPassword').value ? null : {'mismatch' : true};
   }
 
   register(){
-    //register method ekata ape model eka yawanawa
-    this.authService.register(this.model).subscribe((res)=>{
-      console.log("Registration Successful");
-      this.alertify.success("Registration Successful");
-    }, error=>{
-      //console.log(error);
-      this.alertify.error(error);
-    });
-    this.registerForm.reset();
-    //console.log(this.model);
+    // //register method ekata ape model eka yawanawa
+    // this.authService.register(this.model).subscribe((res)=>{
+    //   console.log("Registration Successful");
+    //   this.alertify.success("Registration Successful");
+    // }, error=>{
+    //   //console.log(error);
+    //   this.alertify.error(error);
+    // });
+    // this.registerForm.reset();
+    // //console.log(this.model);
+
+    console.log(this.registerFormReactive.value);
   }
 
   cancel(){
