38567c94514be643fcd9ca2ddf4fa613b7007933
Gagana Gavesh <gaveshgamage@gmail.com>
2021-02-26 15:53:20 +0530

    list component configured eith the like functionality

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Pagination } from '../_models/Pagination';
+import { User } from '../_models/User';
+import { AlertifyService } from '../_services/alertify.service';
+import { AuthService } from '../_services/auth.service';
+import { UserService } from '../_services/user.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +13,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  users: User[];
+  pagination: Pagination
+  likesParam: string;
+
+  constructor(
+    private authService: AuthService, 
+    private userService: UserService, 
+    private alertify: AlertifyService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.data.subscribe(data =>{
+      //route rkr data enawa, e data tynne key value paires widiyata, resolver eken eka return eka access karanna pluwan 
+      //key eka haraha thama, ex ['users']
+      //basicaly resolver eke return eka enne route data wala ['users'] kiyana key eka yatathata
+      this.users = data['users'].result;
+      this.pagination = data['users'].pagination;
+    })
+    this.likesParam = 'Likers'
+  }
+
+  //this method loads next batch of users
+  loadUsers(){
+    this.userService.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage, null, this.likesParam).subscribe(
+      response => {
+        this.users = response.result;
+        this.pagination = response.pagination;
+      }, error => {
+        this.alertify.error(error);
+      }
+    )
+    console.log(this.likesParam);
+  }
+
+  pageChanged(event: any): void {
+    // console.log('Page changed to: ' + event.page);
+    // console.log('Number items per page: ' + event.itemsPerPage);
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
   }
 
 }
