381a702b7bd4f8d4f14aafd2a9f42ebf6d36ee30
Gagana Gavesh <gaveshgamage@gmail.com>
2021-02-26 15:54:09 +0530

    enable hiting like button and like functionality

--- src/app/members/member-card/member-card.component.ts
+++ src/app/members/member-card/member-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { User } from 'src/app/_models/User';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { AuthService } from 'src/app/_services/auth.service';
+import { UserService } from 'src/app/_services/user.service';
 
 @Component({
   selector: 'app-member-card',
@@ -10,9 +13,22 @@ export class MemberCardComponent implements OnInit {
   //each card component contains a single member.
   @Input() user : User;
   
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private userService : UserService, 
+    private alertify : AlertifyService) { }
 
   ngOnInit(): void {
+  }
+
+  sendLike(recipientId: number){
+    this.userService.sendLike(this.authService.decodedToken.nameid, recipientId).subscribe(data =>{
+      this.alertify.success('You have liked ' + this.user.knownAs);//users la eka eka gane neh 
+      //ekata for loolp eka haraha load wenne, e nisa eka user kenekge details ganna eka amaru nee, 
+      //kelinma user. kiyala danawa
+    }, error => {
+      this.alertify.error(error);
+    });
   }
 
 }
